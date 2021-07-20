ced33535c5648948851c488b8fb00947b4e61732
aurphillus <aniketpaul2326@gmail.com>
2021-07-20 07:37:26 +0530

    Added Scaffold and Network Image

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello World!',
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Center(
+            child: Text(
+              'I am Rich!',
+            ),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+              'https://lumiere-a.akamaihd.net/v1/images/Darth-Vader_6bda9114.jpeg',
+            ),
+          ),
         ),
       ),
     ),
